c1eb68b48b28741a1a183952a2285bc5816d7f24
MMarzoo <marzouk12378@gmail.com>
2024-05-20 22:20:30 +0300

    adding files

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,14 +9,13 @@ import sys
 
 REST_API = "https://jsonplaceholder.typicode.com"
 
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
             req = requests.get('{}/users/{}'.format(REST_API, id)).json()
             task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp-name = req.get('name')
+            emp_name = req.get('name')
             tasks = list(filter(lambda x: x.get('userId') == id, task_req))
             completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
             print(
@@ -28,4 +27,4 @@ if __name__ == '__main__':
             )
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
-                    print('\t {}'.foramt(task.get('title')))
+                    print('\t {}'.format(task.get('title')))
