2178ad5d0a4761ffd65fa16b375c5dd062c506ce
MMarzoo <marzouk12378@gmail.com>
2024-06-04 19:04:17 +0300

    adding files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-
-"""This module contains number_of_subscribers functions"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers (not active users, total subscribers)
-    for a given subreddit.
-    """
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-               AppleWebKit/537.36 (KHTML, like Gecko)\
-               Chrome/125.0.0.0 Safari/537.36"
-    }
-
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
