1ba13aa330fa51907172780d8342c63ea7fc8d77
MMarzoo <marzouk12378@gmail.com>
2024-06-04 20:08:27 +0300

    adding files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,8 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-        }
+        "User-Agent": "Mozilla/5.0(Window NT 10.0; Win64; x64)Chrome/125.0.0.0"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
